e552da47af6bf7c87c59931e47be1d3b716a58e7
Jeremia Kevin Raja Parluhutan Siahaan <jkrparluhutan@cermati.com>
2020-06-27 00:11:08 +0700

    init asset

--- main.go
+++ main.go
@@ -1,63 +1,24 @@
+// main.go
+
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+import "github.com/gin-gonic/gin"
 
-	"github.com/gorilla/mux"
-)
-
-// Article comment
-type Article struct {
-	ID      string `json:"Id"`
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
-
-// Articles yeah
-// let's declare a global Articles array
-// that we can then populate in our main function
-// to simulate a database
-var Articles []Article
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnAllArticles")
-	json.NewEncoder(w).Encode(Articles)
-}
-
-func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["id"]
-
-	fmt.Fprintf(w, "Key: "+key)
-}
-
-func handleRequests() {
-	// creates a new instance of a mux router
-	myRouter := mux.NewRouter().StrictSlash(true)
-	// replace http.HandleFunc with myRouter.HandleFunc
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/articles", returnAllArticles)
-	myRouter.HandleFunc("/articles/{id}", returnSingleArticle)
-	// finally, instead of passing in nil, we want
-	// to pass in our newly created router as the second
-	// argument
-	log.Fatal(http.ListenAndServe(":10000", myRouter))
-}
+var router *gin.Engine
 
 func main() {
-	fmt.Println("Rest API v2.0 - Mux Routers")
-	Articles = []Article{
-		Article{ID: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{ID: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-	}
-	handleRequests()
+
+	// Set the router as the default one provided by Gin
+	router = gin.Default()
+
+	// Process the templates at the start so that they don't have to be loaded
+	// from the disk again. This makes serving HTML pages very fast.
+	router.LoadHTMLGlob("templates/*")
+
+	// Initialize the routes
+	initializeRoutes()
+
+	// Start serving the application
+	router.Run()
+
 }
